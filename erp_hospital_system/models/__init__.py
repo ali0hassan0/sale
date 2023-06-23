7d8f5bcc2d4ee936ad285fd37058e9c3682016a0
ali0hassan0 <137399245+ali0hassan0@users.noreply.github.com>
2023-06-23 15:11:53 +0500

    Delete erp_hospital_system directory

--- erp_hospital_system/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import hospital_management, doctor_appoint, \
-    medicine_hospital, configure_hospital, sale_order, technical_hospital, hospital_res_config_setting
--- erp_hospital_system/models/hospital_res_config_setting.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    cancel_days = fields.Integer(string="Cancel days", config_parameter="hospital_management.cancel_days")
